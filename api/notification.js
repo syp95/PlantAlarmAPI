1e33c11cea136d7168a0f3a16fd646d40d6078f2
syp <sy95_055@naver.com>
2022-11-04 21:52:44 +0900

    feat(notification): 스케줄러를 사용한 물이 필요한 식물 자동알람 구현

--- api/notification.js
+++ api/notification.js
@@ -5,12 +5,14 @@ const db = require('../models');
 
 const { Notification } = db;
 const webPush = require('web-push');
+const { Plant } = require('../models');
 
 require('dotenv').config();
 const SUBJECT = `mailto:${process.env.WEB_PUSH_EMAIL}`;
 const vapidKeys = webPush.generateVAPIDKeys();
-const VAPID_PUBLIC = vapidKeys.publicKey;
-const VAPID_PRIVATE = vapidKeys.privateKey;
+const VAPID_PUBLIC =
+    'BKJB4jb5AkW60f2SJIrXTbAtpomru-zK6E-L1oKcOzEw1MsSyVhlPKLNWVOgItLPBM0k2ukKAxNDHTh9ApHBFhY';
+const VAPID_PRIVATE = 'cqaGP89oYvV_Hn2CjSOWKUs4JIwjEky9oS06NUhpjX8';
 
 webPush.setVapidDetails(SUBJECT, VAPID_PUBLIC, VAPID_PRIVATE);
 
@@ -62,6 +64,46 @@ router.delete('/subscription/:userid', async (req, res) => {
     }
 });
 
+// 스케줄러 매일 아침 7시 30분에 등록.
+scheduleJob('30 07 * * *', () => {
+    let plantArr = [];
+    Plant.findAll({ raw: true }).then((res) => (plantArr = res));
+    setTimeout(() => {
+        plantArr.map(async (res) => {
+            let last = res.lastWateringDate.getTime();
+            let water = res.wateringDate * (1000 * 60 * 60 * 24);
+            let today = new Date(new Date().setHours(0, 0, 0, 0)).getTime();
+            let restDay = last + water - today;
+
+            if (0 >= restDay) {
+                const targetUser = await Notification.findOne({
+                    where: { userid: res.creatorId },
+                    raw: true,
+                });
+                console.log(targetUser);
+                if (targetUser) {
+                    const messageData = {
+                        title: '식물 알람 | Plant Alarm',
+                        message: `${res.plantName}에게 물을 줘야해요!`,
+                    };
+                    webpush
+                        .sendNotification(
+                            targetUser.subscription,
+                            JSON.stringify(messageData),
+                            { TTL: 3600 * 12 },
+                        )
+                        .then((pushServiceRes) =>
+                            console.log(pushServiceRes.statusCode),
+                        )
+                        .catch((error) => {
+                            console.log(error?.statusCode ?? 500);
+                        });
+                }
+            }
+        });
+    }, 2000);
+});
+
 router.post('/send-push-notification', async (req, res) => {
     const { targetId: targetUserId, message, date } = req.body ?? {};
 
@@ -74,32 +116,21 @@ router.post('/send-push-notification', async (req, res) => {
             title: '식물 알람 | Plant Alarm',
             message,
         };
-        const dateArr = date.split('-'); // 날짜로 변환해주기
-        const alarmDate = new Date(
-            dateArr[0],
-            dateArr[1] - 1,
-            dateArr[2],
-            dateArr[3],
-            dateArr[4],
-            dateArr[5],
-        );
 
-        scheduleJob(alarmDate, () => {
-            console.log('push alarm');
-            webpush
-                .sendNotification(
-                    targetUser.subscription,
-                    JSON.stringify(messageData),
-                    { TTL: 3600 * 12 },
-                )
-                .then((pushServiceRes) =>
-                    res.status(pushServiceRes.statusCode).end(),
-                )
-                .catch((error) => {
-                    logger.error('POST /send-push', { error });
-                    res.status(error?.statusCode ?? 500).end();
-                });
-        });
+        console.log('push alarm');
+        webpush
+            .sendNotification(
+                targetUser.subscription,
+                JSON.stringify(messageData),
+                { TTL: 3600 * 12 },
+            )
+            .then((pushServiceRes) =>
+                res.status(pushServiceRes.statusCode).end(),
+            )
+            .catch((error) => {
+                logger.error('POST /send-push', { error });
+                res.status(error?.statusCode ?? 500).end();
+            });
     } else {
         res.status(404).end();
     }
